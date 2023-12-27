9a5d18eaaf3ef9ed6006fa03e78560542dd429a1
Jorge Pérez Bárcenas <jorge.barcenas@paramount.com>
2023-12-27 19:41:15 -0300

    Adding the ChargingStation Response object

---
+++ src/main/java/com/myev/charge/payload/ChargingStationResponse.java
@@ -0,0 +1,19 @@
+package com.myev.charge.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChargingStationResponse {
+    private List<ChargingStationVO> content;
+    private int pageNo;
+    private int pageSize;
+    private long totalElements;
+    private int totalPages;
+    private boolean last;
+}
