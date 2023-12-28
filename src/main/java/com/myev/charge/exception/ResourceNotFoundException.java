3ea29f34a067e65a5e4b5305cc18c3ac63563614
Jorge Pérez Bárcenas <jorge.barcenas@paramount.com>
2023-12-28 12:12:24 -0300

    Adding the ResourceNotFound exception class

---
+++ src/main/java/com/myev/charge/exception/ResourceNotFoundException.java
@@ -0,0 +1,27 @@
+package com.myev.charge.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    private String resourceName;
+    private String fieldName;
+    private long fieldValue;
+
+    public ResourceNotFoundException(String resourceName, String fieldName, long fieldValue) {
+        // Post not found with id : 1
+        super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+
+    public String getResourceName() {
+        return resourceName;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public long getFieldValue() {
+        return fieldValue;
+    }
+}
