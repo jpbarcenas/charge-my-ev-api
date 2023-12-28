387a08ae2d6f86befdfc6e51c46e5488a10ca126
Jorge Pérez Bárcenas <jorge.barcenas@paramount.com>
2023-12-28 16:38:56 -0300

    Adding the ChargingPointController Test class (not implemented)

---
+++ src/test/java/com/myev/charge/controller/ChargingPointControllerTest.java
@@ -0,0 +1,38 @@
+package com.myev.charge.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChargingPointControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createPoint() {
+    }
+
+    @Test
+    void getAllByStationId() {
+    }
+
+    @Test
+    void getPointByIdAndStationId() {
+    }
+
+    @Test
+    void updatePoint() {
+    }
+
+    @Test
+    void deletePoint() {
+    }
+}
