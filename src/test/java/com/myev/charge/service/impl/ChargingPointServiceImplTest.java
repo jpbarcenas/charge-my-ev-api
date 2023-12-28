bb3777f8cafe0da85d05b553ced2b93a46e65c64
Jorge Pérez Bárcenas <jorge.barcenas@paramount.com>
2023-12-28 16:41:16 -0300

    Adding the ChargingPointServiceImpl Test class (not implemented)

---
+++ src/test/java/com/myev/charge/service/impl/ChargingPointServiceImplTest.java
@@ -0,0 +1,38 @@
+package com.myev.charge.service.impl;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChargingPointServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void doCreate() {
+    }
+
+    @Test
+    void doGetAllByStationId() {
+    }
+
+    @Test
+    void getByIdAndStationId() {
+    }
+
+    @Test
+    void doUpdate() {
+    }
+
+    @Test
+    void doDelete() {
+    }
+}
