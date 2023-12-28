dffdd435d4a1e4f404b00877a91d60ac6a6d5c11
Jorge Pérez Bárcenas <jorge.barcenas@paramount.com>
2023-12-28 13:27:03 -0300

    Implemented the private 'mapToDto' and 'mapToEntity' for the ChargingPoint Service class

--- src/main/java/com/myev/charge/service/impl/ChargingPointServiceImpl.java
+++ src/main/java/com/myev/charge/service/impl/ChargingPointServiceImpl.java
@@ -1,7 +1,11 @@
 package com.myev.charge.service.impl;
 
+import com.myev.charge.domain.ChargerType;
+import com.myev.charge.domain.ChargingPoint;
+import com.myev.charge.payload.ChargerTypeDto;
 import com.myev.charge.payload.ChargingPointDto;
 import com.myev.charge.payload.ChargingPointResponse;
+import com.myev.charge.payload.ChargingStationDto;
 import com.myev.charge.repository.ChargingPointRepository;
 import com.myev.charge.repository.ChargingStationRepository;
 import com.myev.charge.service.IChargingPointService;
@@ -40,5 +44,46 @@ public class ChargingPointServiceImpl implements IChargingPointService {
     @Override
     public void doDelete(long id) {
 
+    }
+
+    private ChargingPointDto mapToDto(ChargingPoint chargingPoint) {
+
+        // getting the charger type from charging point
+        ChargerTypeDto chargerTypeDto = new ChargerTypeDto();
+        chargerTypeDto.setType(chargingPoint.getChargerType().getType());
+        chargerTypeDto.setPrice(chargingPoint.getChargerType().getPrice());
+        chargerTypeDto.setSpeed(chargingPoint.getChargerType().getSpeed());
+
+        // getting the charging station from charging point
+        ChargingStationDto chargingStationDto = new ChargingStationDto();
+        chargingStationDto.setId(chargingPoint.getChargingStation().getId());
+        chargingStationDto.setNumberOfChargingPoints(chargingPoint.getChargingStation().getNumberOfChargingPoints());
+        chargingStationDto.setStatus(chargingPoint.getChargingStation().getStatus());
+
+        // create Dto object
+        ChargingPointDto chargingPointDto = new ChargingPointDto();
+        chargingPointDto.setId(chargingPoint.getId());
+        chargingPointDto.setPowerLevel(chargingPoint.getPowerLevel());
+        chargingPointDto.setChargerType(chargerTypeDto);
+        chargingPointDto.setChargingStation(chargingStationDto);
+
+        return chargingPointDto;
+    }
+
+    private ChargingPoint mapToEntity(ChargingPointDto chargingPointDto) {
+
+        // getting the charger type from charging point
+        ChargerType chargerType = new ChargerType();
+        chargerType.setType(chargingPointDto.getChargerType().getType());
+        chargerType.setPrice(chargingPointDto.getChargerType().getPrice());
+        chargerType.setSpeed(chargingPointDto.getChargerType().getSpeed());
+
+        // create Dto object
+        ChargingPoint chargingPoint = new ChargingPoint();
+        chargingPoint.setId(chargingPointDto.getId());
+        chargingPoint.setPowerLevel(chargingPointDto.getPowerLevel());
+        chargingPoint.setChargerType(chargerType);
+
+        return chargingPoint;
     }
 }
