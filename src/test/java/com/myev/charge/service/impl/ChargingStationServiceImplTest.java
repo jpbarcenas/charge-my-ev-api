bb4543e354ad8ad631d33ce1dbfb2c6c33ae1e68
Jorge Pérez Bárcenas <jorge.barcenas@paramount.com>
2023-12-28 12:26:13 -0300

    Adding Test class for ChargingStationService with base structure

---
+++ src/test/java/com/myev/charge/service/impl/ChargingStationServiceImplTest.java
@@ -0,0 +1,38 @@
+package com.myev.charge.service.impl;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChargingStationServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void testDoCreate() {
+    }
+
+    @Test
+    void testDoGetAll() {
+    }
+
+    @Test
+    void testDoGetById() {
+    }
+
+    @Test
+    void testDoUpdate() {
+    }
+
+    @Test
+    void testDoDelete() {
+    }
+}
