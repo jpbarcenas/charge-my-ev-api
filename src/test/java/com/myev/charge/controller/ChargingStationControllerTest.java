f83aa91db0733ffd555eafe811b2f9af4e313733
Jorge Pérez Bárcenas <jorge.barcenas@paramount.com>
2023-12-28 16:37:14 -0300

    Adding the ChargingStationController Test class (not implemented)

---
+++ src/test/java/com/myev/charge/controller/ChargingStationControllerTest.java
@@ -0,0 +1,38 @@
+package com.myev.charge.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChargingStationControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createStation() {
+    }
+
+    @Test
+    void getAllStations() {
+    }
+
+    @Test
+    void getStationById() {
+    }
+
+    @Test
+    void updateStation() {
+    }
+
+    @Test
+    void deleteStation() {
+    }
+}
